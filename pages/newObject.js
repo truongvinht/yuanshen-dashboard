900ffc4fb484ab2b0d456ab8be335da6dbff05bb
Vinh Tran <truongvinht@gmail.com>
2022-01-02 11:07:48 +0100

    update security access in creating new objects

--- pages/newObject.js
+++ pages/newObject.js
@@ -1,6 +1,7 @@
 import ObjectForm from '../components/ObjectForm'
 import Login from '../components/Login'
 import nookies from 'nookies'
+import securityChecker from '../lib/securityChecker'
 
 const NewObject = ({hasReadPermission = false}) => {
   if (!hasReadPermission) {
@@ -25,7 +26,7 @@ export async function getServerSideProps(ctx) {
   // Parse
   const cookies = nookies.get(ctx);
 
-  return { props: { hasReadPermission: cookies.ys_login_pwd === 'value'} }
+  return { props: { hasReadPermission: securityChecker(cookies.ys_login_pwd)} }
 }
 
 export default NewObject
