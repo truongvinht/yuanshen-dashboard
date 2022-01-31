68bf2d0640c846a90015e4e4eac3e88f93e07757
Vinh Tran <truong.tran@mhp.com>
2022-01-31 07:52:10 +0100

    add errorpage

---
+++ components/ErrorPage.js
@@ -0,0 +1,10 @@
+const ErrorPage = () => {
+    return (
+        <div>
+          <h1>Daten derzeitig nicht abrufbar. Verbindungsfehler...</h1>
+          
+        </div>
+    );
+};
+
+export default ErrorPage
