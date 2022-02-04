22fe0e41a20063ecc6eafcf7c69eeddf3fcf120c
Vinh Tran <truong.tran@mhp.com>
2022-02-04 18:46:35 +0100

    adjust generator

--- generateModel.sh
+++ generateModel.sh
@@ -23,12 +23,12 @@ if test $# -gt 0; then
     schema=$model
     schema+="Schema"
 
-    echo -e "/* $schema will correspond to a collection in your MongoDB database. */\n" >> "$pathToFile"
+    echo -e "/* $schema will correspond to a collection in your MongoDB database. */" >> "$pathToFile"
 
     echo -e "const $schema = new mongoose.Schema({\n" >> "$pathToFile"
 
     echo -e "})\n" >> "$pathToFile"
-    echo -e "export default mongoose.models.$model || mongoose.model('$model', $schema)\n" >> "$pathToFile"
+    echo -e "export default mongoose.models.$model || mongoose.model('$model', $schema)" >> "$pathToFile"
 
     # create api
     apifolder=`node -p "\"$model\".toLowerCase()"`
@@ -39,25 +39,25 @@ if test $# -gt 0; then
     touch "$apiFile"
 
     echo -e "import apiHandler from '../../../lib/apiHandler'" >> "$apiFile"
-    echo -e "import Model from '../../../models/$model'\n\n" >> "$apiFile"
+    echo -e "import Model from '../../../models/$model'\n" >> "$apiFile"
     echo -e "export default async function handler(req, res) {" >> "$apiFile"
-    echo -e "    await apiHandler(Model, req, res)\n}\n" >> "$apiFile"
+    echo -e "    await apiHandler(Model, req, res)\n}" >> "$apiFile"
 
     # create page index
     mkdir "./pages/$apifolder"
     pageFile="./pages/$apifolder/index.js"
     touch "$pageFile"
 
-    echo -e "import dbConnect from '../../lib/dbConnect'\n" >> "$pageFile"
-    echo -e "import $model from '../../models/$model'\n" >> "$pageFile"
-    echo -e "import Header from '../../components/Header'\n\n" >> "$pageFile"
-    echo -e "const $model = () => (\n" >> "$pageFile"
-    echo -e "    return (\n" >> "$pageFile"
-    echo -e "    <div>\n" >> "$pageFile"
-    echo -e "    <Header headerTitle={\"$model\"}/>\n" >> "$pageFile"
-    echo -e "    </div>\n" >> "$pageFile"
-    echo -e "    )\n" >> "$pageFile"
-    echo -e "};\n\n" >> "$pageFile"
+    echo -e "import dbConnect from '../../lib/dbConnect'" >> "$pageFile"
+    echo -e "import $model from '../../models/$model'" >> "$pageFile"
+    echo -e "import Header from '../../components/Header'\n" >> "$pageFile"
+    echo -e "const $model = () => {" >> "$pageFile"
+    echo -e "    return (" >> "$pageFile"
+    echo -e "    <div>" >> "$pageFile"
+    echo -e "        <Header headerTitle={\"$model\"}/>" >> "$pageFile"
+    echo -e "    </div>" >> "$pageFile"
+    echo -e "    );" >> "$pageFile"
+    echo -e "};\n" >> "$pageFile"
     echo -e "export default $model\n" >> "$pageFile"
 
 
