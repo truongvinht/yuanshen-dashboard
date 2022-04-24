e969c217b0ee75bdce4e90c2366c82049bfb1255
Truong Vinh Tran <truongvinht@gmail.com>
2022-04-24 20:03:12 +0200

    update pull list

--- pages/pulls/index.js
+++ pages/pulls/index.js
@@ -1,4 +1,4 @@
-import {AgGridColumn, AgGridReact} from 'ag-grid-react';
+import { AgGridColumn, AgGridReact } from 'ag-grid-react';
 import dbConnect from '../../lib/dbConnect'
 import Pull from '../../models/Pull'
 import PullObject from '../../models/PullObject'
@@ -8,99 +8,105 @@ import { useState } from 'react'
 import Actionbar from '../../components/Actionbar'
 
 
-const pulls = ({pulls, actions = {}}) => {
+const pulls = ({ pulls, actions = {} }) => {
   const router = useRouter()
   const [gridApi, setGridApi] = useState(null);
 
   const onGridReady = (params) => {
     setGridApi(params.api);
   };
-//router.push('/objects')
+  //router.push('/objects')
   const onSelectionChanged = () => {
     const selectedRows = gridApi.getSelectedRows();
     router.push(`/pulls/${selectedRows[0]._id}`)
   };
 
+  const valueFormatter = params => {
+    const options = { year: 'numeric', month: 'numeric', day: 'numeric' , hour: 'numeric', minute: 'numeric',second: 'numeric' };
+    let date = params.value;
+    
 
+    return new Date(date).toLocaleDateString('de-DE', options);
+  }
 
   const dynamicCellStyle = params => {
 
     console.log(params);
     if (params.data.rating === '4') {
-        //mark police cells as red
-        return {color: 'purple','font-weight': 'bold', backgroundColor: 'AliceBlue'};
-    }else if (params.data.rating === '5') {
-        //mark police cells as red
-        return {color: 'orange', 'font-weight': 'bold',backgroundColor: 'AliceBlue'};
+      //mark police cells as red
+      return { color: 'purple', 'font-weight': 'bold', backgroundColor: 'AliceBlue' };
+    } else if (params.data.rating === '5') {
+      //mark police cells as red
+      return { color: 'orange', 'font-weight': 'bold', backgroundColor: 'AliceBlue' };
     }
-    return {color: 'black', backgroundColor: 'white'};
+    return { color: 'black', backgroundColor: 'white' };
   };
 
   return (
-    <div>
+    <div style={{height: '90%' ,width: '100%', position: 'fixed'}}>
       <Head>
         <title>Yuanshen-Dashboard</title>
       </Head>
       <h1>Überblick der Ziehungen ({pulls.length})</h1>
       <Actionbar actions={actions} />
-      <div className="ag-theme-alpine" style={{height: 400, width: 800}}>
-          <AgGridReact
-              rowData={pulls}
-              rowSelection={'single'}
-              onGridReady={onGridReady}
-              onSelectionChanged={onSelectionChanged}
-              rowStyle={'black'}
+      <div className="ag-theme-alpine" style={{ height: '80%',width: '100%' }}>
+        <AgGridReact
+          rowData={pulls}
+          rowSelection={'single'}
+          onGridReady={onGridReady}
+          onSelectionChanged={onSelectionChanged}
+          rowStyle={'black'}
 
->
-              <AgGridColumn field="banner" cellStyle={dynamicCellStyle}></AgGridColumn>
-              <AgGridColumn headerName="Ziehung" field="object_ref" cellStyle={dynamicCellStyle}></AgGridColumn>
-              <AgGridColumn headerName="Zeit" field="eventTime" cellStyle={dynamicCellStyle}></AgGridColumn>
-              <AgGridColumn headerName="Index" maxWidth="80" field="order_index" cellStyle={dynamicCellStyle}></AgGridColumn>
-              <AgGridColumn headerName="UID" field="uid" cellStyle={dynamicCellStyle}></AgGridColumn>
+        >
+          <AgGridColumn field="banner" cellStyle={dynamicCellStyle}></AgGridColumn>
+          <AgGridColumn headerName="Ziehung" field="object_ref" cellStyle={dynamicCellStyle} resizable="true"></AgGridColumn>
+          <AgGridColumn headerName="Zeit" field="eventTime" valueFormatter={valueFormatter} cellStyle={dynamicCellStyle}></AgGridColumn>
+          <AgGridColumn headerName="Index" maxWidth="80" field="order_index" cellStyle={dynamicCellStyle}></AgGridColumn>
 
-          </AgGridReact>
+        </AgGridReact>
       </div>
     </div>
   );
 };
 // <AgGridColumn headerName="Rarität" maxWidth="80" field="rating"></AgGridColumn>
 
+//<AgGridColumn headerName="UID" field="uid" cellStyle={dynamicCellStyle}></AgGridColumn>
 export async function getServerSideProps() {
 
-    // start db connection
-    await dbConnect()
+  // start db connection
+  await dbConnect()
 
-    const objResult = await PullObject.find({})
-    const objects = objResult.map((obj) => {
-      const pullObj = obj.toObject()
-      pullObj._id = pullObj._id.toString()
-      return pullObj
-    })
+  const objResult = await PullObject.find({})
+  const objects = objResult.map((obj) => {
+    const pullObj = obj.toObject()
+    pullObj._id = pullObj._id.toString()
+    return pullObj
+  })
 
-    /* find all the data in our database */
-    const result = await Pull.find({}).sort([["eventTime", 1], ["index", 1]])
-    const pulls = result.map((doc) => {
-        let pull = doc.toObject()
-        
-        pull._id = pull._id.toString()
+  /* find all the data in our database */
+  const result = await Pull.find({}).sort([["eventTime", 1], ["index", 1]])
+  const pulls = result.map((doc) => {
+    let pull = doc.toObject()
 
-        for (const pullObj of objects) {
-          if (pullObj._id === pull.object_ref) {
-            pull.object_ref = pullObj.name;
-            pull.rating = `${pullObj.rating}`;
-          }
-        }
+    pull._id = pull._id.toString()
 
-        pull.eventTime = pull.eventTime.toLocaleString()
-        return pull
-    })
+    for (const pullObj of objects) {
+      if (pullObj._id === pull.object_ref) {
+        pull.object_ref = pullObj.name;
+        pull.rating = `${pullObj.rating}`;
+      }
+    }
+
+    pull.eventTime = pull.eventTime.toLocaleString()
+    return pull
+  })
 
   // actions
-  let actions = [{'param_ref':'/pulls/new', 'param_as':'/pulls/new', 'param_title':'Single', isEdit:true},
-  {'param_ref':'/pulls/newMulti', 'param_as':'/pulls/newMulti', 'param_title':'Multi', isEdit:true}
-];
+  let actions = [{ 'param_ref': '/pulls/new', 'param_as': '/pulls/new', 'param_title': 'Single', isEdit: true },
+  { 'param_ref': '/pulls/newMulti', 'param_as': '/pulls/newMulti', 'param_title': 'Multi', isEdit: true }
+  ];
 
-    return { props: { pulls: pulls , actions: actions} }
+  return { props: { pulls: pulls, actions: actions } }
 }
 
 export default pulls
